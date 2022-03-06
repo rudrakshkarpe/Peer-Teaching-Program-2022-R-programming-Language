61ab0305d1ccf29c9d1dad3a8c20037c7fe9f50d
rudraskhkarpe <rudraksh.karpe.cs@ghrcem.raisoni.net>
2022-03-06 23:15:29 +0530

    updated using the new R Notebooks

---
+++ Types_of_variables.R
@@ -0,0 +1,38 @@
+# interger
+
+x <- 2 
+
+# to make it interger keep it like 2L
+
+typeof(x)
+
+# double
+
+y <- 2.5  # decimal point 
+typeof(y)
+
+
+# double mode is default mode in
+# R programming langauge
+
+# complex
+
+z <- 3 +2i
+  
+typeof(z)
+
+# character
+
+a <- "Rudraksh"
+
+typeof(a)
+
+# logical 
+
+q1 <- T
+
+typeof(q1)
+
+q2 <- FALSE
+
+typeof(q2)
